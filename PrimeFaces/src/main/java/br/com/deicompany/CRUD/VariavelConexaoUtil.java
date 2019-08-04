91d2da0bd7a4d59d954aa11c4f9e17a32bc0c492
SergioBastos <SergioBastos@192.168.15.15>
2019-08-04 00:48:12 -0300

    Primeiro Envio

---
+++ PrimeFaces/src/main/java/br/com/deicompany/CRUD/VariavelConexaoUtil.java
@@ -0,0 +1,10 @@
+package br.com.deicompany.CRUD;
+
+/* 
+ * NOME DO CAMINHO DO JNDI datasource TomCat 
+ */
+public class VariavelConexaoUtil {
+
+	public static String JAVA_COMP_ENV_JDBC_DATASOURCE = "java:/comp/env/jdbc/datasource";
+	
+}
